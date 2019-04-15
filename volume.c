81cedd88888364ff85aad638b9efe496c4f2cd2a
Wenhan Jin <tigerjin4@berkeley.edu>
2019-04-15 14:57:04 -0700

    dd

--- volume.c
+++ volume.c
@@ -71,8 +71,8 @@ void copy_volume(volume_t *dest, volume_t *src) {
     for (int x = 0; x < dest->width; x++) {
         for (int y = 0; y < dest->height; y++) {
             for(int d = 0; d < dest->depth / 4 * 4; d += 4) {
-                __m256d value = _mm256_load_pd(src->weights+d);
-                _mm256_store_pd(dest->weights+d, value);
+                __m256d value = _mm256_load_pd(src->weights+(((src->width * y) + x) * src->depth + d));
+                _mm256_store_pd(src->weights+(((src->width * y) + x) * src->depth + d), value);
             }
             for (int d = dest->depth / 4 * 4; d < dest->depth; d++) {
                 volume_set(dest, x, y, d, volume_get(src, x, y, d));
