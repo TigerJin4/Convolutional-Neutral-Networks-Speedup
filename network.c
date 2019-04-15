0ac0780fc0992ad69bb4f650ed37caa2f0c83a1b
Wenhan Jin <tigerjin4@berkeley.edu>
2019-04-15 10:08:48 -0700

    second try

--- network.c
+++ network.c
@@ -112,10 +112,11 @@ batch_t *make_batch(network_t *net, int size) {
 //        }
         // Unrolling
         for(int j = 0; j < size/4*4; j += 4){
-            out[i][j] = make_volume(net->layers[i]->width, net->layers[i]->height, net->layers[i]->depth, 0.0);
-            out[i][j+1] = out[i][j];
-            out[i][j+2] = out[i][j];
-            out[i][j+3] = out[i][j];
+            int output = make_volume(net->layers[i]->width, net->layers[i]->height, net->layers[i]->depth, 0.0);
+            out[i][j] = output;
+            out[i][j+1] = output;
+            out[i][j+2] = output;
+            out[i][j+3] = output;
         }
         for(int j = size/4*4; j < size; j++){
             out[i][j] = make_volume(net->layers[i]->width, net->layers[i]->height, net->layers[i]->depth, 0.0);
