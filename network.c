3709837bb15640b288cfc0fb8dbc82b75fa1b93e
Wenhan Jin <tigerjin4@berkeley.edu>
2019-04-15 21:59:00 -0700

    dd

--- network.c
+++ network.c
@@ -152,12 +152,24 @@ void net_classify(network_t *net, volume_t **input, double **likelihoods, int n)
     batch_t *b = make_batch(net, 1);
 
 // Original
+//    for (int i = 0; i < n; i++) {
+//        copy_volume(b[0][0], input[i]);
+//        net_forward(net, b, 0, 0);
+//        for (int j = 0; j < NUM_CLASSES; j++) {
+//            likelihoods[i][j] = b[11][0]->weights[j];
+//        }
+//    }
+
+#pragma omp parallel
+    {
     for (int i = 0; i < n; i++) {
         copy_volume(b[0][0], input[i]);
         net_forward(net, b, 0, 0);
+        #pragma omp for
         for (int j = 0; j < NUM_CLASSES; j++) {
             likelihoods[i][j] = b[11][0]->weights[j];
         }
+    }
     }
 
 
